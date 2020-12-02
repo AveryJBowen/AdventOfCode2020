4e9ab9fc426077bef2ada0dafa130a94576e3f13
Avery Bowen <averyjbowen@gmail.com>
2020-12-02 18:47:45 -0500

    Fixed multiple outputs

--- AOC2020_day1.py
+++ AOC2020_day1.py
@@ -12,23 +12,34 @@ for i in range(0, len(content)):
     content[i] = int(content[i])
 
 content.sort()
-for i in range(0, len(content)):
-    number1 = content[i]
-    tofind = 2020 - content[i]
-    if tofind in content:
-        getPartOne(number1, tofind)
-    else:
-        i += 1
+part1Found = False
+part2Found = False
 
-for i in range(0, len(content)):
-    number1 = content[i]
-    for j in range(i+1, len(content)):
-        number2 = content[j]
-        sum1 = number1 + number2
-        if sum1 < 2020:
-            tofind = 2020 - sum1
+while part1Found == False:
+    for i in range(0, len(content)):
+        number1 = content[i]
+        tofind = 2020 - content[i]
         if tofind in content:
-            getPartTwo(number1, number2, tofind)
+            part1Found = True
+            getPartOne(number1, tofind)
             break
+        else:
+            i += 1
             
+testNum = 0
+while part2Found == False:
+    for i in range(0, len(content)):
+        number1 = content[i]
+        for j in range(i+1, len(content)):
+            number2 = content[j]
+            sum1 = number1 + number2
+            if sum1 < 2020:
+                tofind = 2020 - sum1
+                if tofind in content:
+                    if testNum == number1 or testNum == number2 or testNum == tofind:
+                        break                  
+                    else:
+                        part2Found = True                        
+                        getPartTwo(number1, number2, tofind)
+                        testNum = number1                       
 input()
