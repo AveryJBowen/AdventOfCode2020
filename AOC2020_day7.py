d36ffdcb1e216c39014430e18af7dc828adeb494
Avery Bowen <averyjbowen@gmail.com>
2020-12-09 17:44:14 -0500

    Day 7, part 1 complete

--- AOC2020_day7.py
+++ AOC2020_day7.py
@@ -23,15 +23,29 @@ def makeBagDict(somefile):
         allBags[outerbag] = innerbagdict
     return allBags
 
-def getinnercount(somedict, bagcolor, count):
-    count = count
-    for x in somedict:
-        if bagcolor in somedict[x]:
-            count += 1
-        
-    return count
+def getParentBagList(bagdict, searchbag):
+    immediateParentList = []
+    for x in bagdict:
+        if searchbag in bagdict[x]:
+            immediateParentList.append(x)
+    return immediateParentList
 
-allInput = makeBagDict('day7input.txt')
-print(getinnercount(allInput, "shiny gold", 0))
+def getParents(searchdict, masterParentList):
+    if len(masterParentList) == 0:
+        for parent in getParentBagList(searchdict, "shiny gold"):
+            masterParentList.append(parent)
+        getParents(searchdict, masterParentList)
+    else:
+        for bag in masterParentList:
+            for parent in getParentBagList(searchdict, bag):
+                if parent not in masterParentList:
+                    masterParentList.append(parent)
+    return masterParentList
+
+
+masterBagDict = makeBagDict('day7input.txt')
+parentBagsList = []
+print("Number of bags that can eventually contain a shiny gold bag: " + str(len(getParents(masterBagDict, parentBagsList))))
+
 input()
 
