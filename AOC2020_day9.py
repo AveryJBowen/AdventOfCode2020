2fc984194f73be97903f6ef43637ce263c1a654e
Avery Bowen <averyjbowen@gmail.com>
2020-12-14 17:04:59 -0500

    Day 9, part 2 complete

--- AOC2020_day9.py
+++ AOC2020_day9.py
@@ -1,6 +1,10 @@
 def checknums(orig, check):
     return bool(set(orig).intersection(check))
 
+def getSum(sumlist):
+    sumlist.sort()
+    return sumlist[0] + sumlist[-1]
+
 def getPartOne(numberlist):
     for i in range(25, len(numberlist)):
         twentyfive = numberlist[i-25:i]
@@ -11,8 +15,21 @@ def getPartOne(numberlist):
             return numberlist[i]
                 
 
+def getPartTwo(numberlist, numtofind):
+    for i in range(0, len(numberlist)-1):
+        for j in range(i, len(numberlist)):
+            if sum(numberlist[i:j]) == numtofind:
+                return getSum(numberlist[i:j])
+ 
 with open('day9input.txt') as f:
     rawcontent = f.readlines()
 
 rawcontent = [x.strip("\n") for x in rawcontent]
 rawcontent = [int(x) for x in rawcontent]
+
+num = getPartOne(rawcontent)
+num2 = getPartTwo(rawcontent, num)
+
+print("The first number that is not the sum of any 2 of previous 25 numbers is: " + str(num))
+print("The encryption weakness is: " + str(num2))
+input()
